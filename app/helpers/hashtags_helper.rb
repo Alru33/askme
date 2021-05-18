14295f997f98aeb07a1e748df9d6b35e9248bd34
Alru33 <al-snd@yandex.ru>
2021-05-18 12:03:26 +0300

    add link to hashtag

--- app/helpers/hashtags_helper.rb
+++ app/helpers/hashtags_helper.rb
@@ -1,2 +1,7 @@
 module HashtagsHelper
+  def text_with_hashtag_link(text)
+    text.gsub(Hashtag::HASHTAG_REGEX) do |tag|
+      link_to tag, hashtag_path(tag.delete('#').downcase)
+    end.html_safe
+  end
 end
